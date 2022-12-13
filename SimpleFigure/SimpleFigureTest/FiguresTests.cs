cb84f590de4ad96e78d37b7b52852d21264aeb40
DmitryPandyukhin <pandyukhin@gmail.com>
2022-12-13 15:24:06 +0300

    Исправления по замечаниям

--- SimpleFigure/SimpleFigureTest/FiguresTests.cs
+++ SimpleFigure/SimpleFigureTest/FiguresTests.cs
@@ -1,4 +1,5 @@
 ﻿using SimpleFigure;
+using System;
 
 namespace SimpleFigureTest
 {
@@ -6,30 +7,51 @@ namespace SimpleFigureTest
     public class FiguresTests
     {
         /// <summary>
-        /// Тест на отрицательность входных данн
+        /// Тест на отрицательность входных данных круга.
         /// </summary>
         [TestMethod]
-        public void TestNegativeArguments()
+        public void TestCircleNegativeArguments()
         {
-            // круг
             double radius = -5;
             Assert.ThrowsException<ArgumentOutOfRangeException>(() => new Circle(radius));
             Assert.ThrowsException<ArgumentOutOfRangeException>(() => new Figure().СalculateSquare(radius));
+        }
 
-            // треугольник
+        /// <summary>
+        /// Тест на отрицательность входных данных треугольника.
+        /// </summary>
+        [TestMethod]
+        public void TestTriangleNegativeArguments()
+        {
             double a = -5;
             double b = 5;
             double c = 5;
             Assert.ThrowsException<ArgumentOutOfRangeException>(() => new Triangle(a, b, c));
             Assert.ThrowsException<ArgumentOutOfRangeException>(() => new Figure().СalculateSquare(a, b, c));
-            a = 5;
-            b = -5;
-            c = 5;
+        }
+
+        /// <summary>
+        /// Второй тест на отрицательность входных данных треугольника.
+        /// </summary>
+        [TestMethod]
+        public void TestTriangleNegativeArguments2()
+        {
+            double a = 5;
+            double b = -5;
+            double c = 5;
             Assert.ThrowsException<ArgumentOutOfRangeException>(() => new Triangle(a, b, c));
             Assert.ThrowsException<ArgumentOutOfRangeException>(() => new Figure().СalculateSquare(a, b, c));
-            a = 5;
-            b = 5;
-            c = -5;
+        }
+
+        /// <summary>
+        /// Третий тест на отрицательность входных данных треугольника.
+        /// </summary>
+        [TestMethod]
+        public void TestTriangleNegativeArguments3()
+        {
+            double a = 5;
+            double b = 5;
+            double c = -5;
             Assert.ThrowsException<ArgumentOutOfRangeException>(() => new Triangle(a, b, c));
             Assert.ThrowsException<ArgumentOutOfRangeException>(() => new Figure().СalculateSquare(a, b, c));
         }
@@ -83,17 +105,23 @@ namespace SimpleFigureTest
             double b = 8;
             double c = 10;
 
-            // Треугольник прямоугольный.
-            Triangle circle = new(a, b, c);
-            bool? result = circle.CheckRightTriangle();
+            Triangle triangle = new(a, b, c);
+            bool? result = triangle.CheckRightTriangle();
             Assert.AreEqual(true, result);
+        }
 
-            a = 6;
-            b = 8;
-            c = 11;
-            circle = new(a, b, c);
-            result = circle.CheckRightTriangle();
-            // Треугольник непрямоугольный.
+        /// <summary>
+        /// Тест проверки на то, не является ли треугольник прямоугольным.
+        /// </summary>
+        [TestMethod]
+        public void TestCheckNotRightTriangle()
+        {
+            double a = 6;
+            double b = 8;
+            double c = 11;
+
+            Triangle triangle = new(a, b, c);
+            bool? result = triangle.CheckRightTriangle();
             Assert.AreEqual(false, result);
         }
     }
